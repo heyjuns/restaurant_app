3e4248dd1a16f50f160509a31d0fbc98e4f684a0
Juanda Leonardo <juanda.leonardo@nusantara.technology>
2023-05-22 23:40:49 +0700

    apdet reviews

--- lib/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_cubit.dart
+++ lib/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_cubit.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_app/modules/restaurants/data/repositories/restaurant_impl.dart';
+import 'package:restaurant_app/modules/restaurants/domain/entities/restaurant_customer_review_entity.dart';
+import 'package:restaurant_app/modules/restaurants/domain/entities/restaurant_detail_entity.dart';
 
 import 'restaurant_rating_review_state.dart';
 
 class RestaurantRatingReviewCubit extends Cubit<RestaurantRatingReviewState> {
   late RestaurantImpl restaurantImpl;
+
+  late RestaurantDetailEntity restaurant;
+  List<RestaurantCustomerReviewEntity> customerReviews = [];
   RestaurantRatingReviewCubit({
     required this.restaurantImpl,
   }) : super(RestaurantRatingReviewInitState());
@@ -14,9 +19,15 @@ class RestaurantRatingReviewCubit extends Cubit<RestaurantRatingReviewState> {
     emit(RestaurantRatingReviewLoadingState());
     var result = await restaurantImpl.postRestaurantReview(id, name, review);
     if (result.error == false) {
-      emit(RestaurantRatingReviewLoadedState());
+      updateCustomerReviews(result.customerReviews);
     } else {
       emit(RestaurantRatingReviewErrorState(result.message));
     }
+  }
+
+  updateCustomerReviews(List<RestaurantCustomerReviewEntity> data) {
+    emit(RestaurantRatingReviewLoadingState());
+    customerReviews = data;
+    emit(RestaurantRatingReviewLoadedState());
   }
 }
--- lib/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_page.dart
+++ lib/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_page.dart
@@ -1,8 +1,10 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_app/modules/restaurants/domain/entities/restaurant_detail_entity.dart';
 import 'package:restaurant_app/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_cubit.dart';
+import 'package:restaurant_app/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_state.dart';
 
 import '../../data/repositories/restaurant_impl.dart';
 
@@ -26,6 +28,8 @@ class _RestaurantRatingReviewPageState
     cubit = RestaurantRatingReviewCubit(
       restaurantImpl: RestaurantImpl(),
     );
+    cubit.restaurant = widget.restaurant;
+    cubit.updateCustomerReviews(widget.restaurant.customerReviews);
   }
 
   @override
@@ -34,175 +38,190 @@ class _RestaurantRatingReviewPageState
       appBar: AppBar(
         title: const Text('Rating and Review'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      widget.restaurant.rating.toString(),
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineMedium!
-                          .copyWith(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          widget.restaurant.rating > 0
-                              ? Icons.star_rounded
-                              : Icons.star_outline,
-                          size: 12,
-                          color: Colors.amber,
-                        ),
-                        Icon(
-                          widget.restaurant.rating > 1
-                              ? Icons.star_rounded
-                              : Icons.star_outline,
-                          size: 12,
-                          color: Colors.amber,
-                        ),
-                        Icon(
-                          widget.restaurant.rating > 2
-                              ? Icons.star_rounded
-                              : Icons.star_outline,
-                          size: 12,
-                          color: Colors.amber,
-                        ),
-                        Icon(
-                          widget.restaurant.rating > 3
-                              ? Icons.star_rounded
-                              : Icons.star_outline,
-                          size: 12,
-                          color: Colors.amber,
-                        ),
-                        Icon(
-                          widget.restaurant.rating > 4
-                              ? Icons.star_rounded
-                              : Icons.star_outline,
-                          size: 12,
-                          color: Colors.amber,
-                        ),
-                      ],
-                    ),
-                    Text(
-                        '${widget.restaurant.customerReviews.length.toString()} ratings')
-                  ],
-                ),
-                const VerticalDivider(
-                  thickness: 2,
-                ),
-                Expanded(
-                  child: Column(
+      body: BlocProvider(
+        create: (context) => cubit,
+        child: BlocBuilder<RestaurantRatingReviewCubit,
+            RestaurantRatingReviewState>(builder: (context, state) {
+          if (state is RestaurantRatingReviewLoadedState) {
+            return Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
                     children: [
-                      buildRatingSummary(5, Random().nextDouble()),
-                      buildRatingSummary(4, Random().nextDouble()),
-                      buildRatingSummary(3, Random().nextDouble()),
-                      buildRatingSummary(2, Random().nextDouble()),
-                      buildRatingSummary(1, Random().nextDouble()),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            cubit.restaurant.rating.toString(),
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                cubit.restaurant.rating > 0
+                                    ? Icons.star_rounded
+                                    : Icons.star_outline,
+                                size: 12,
+                                color: Colors.amber,
+                              ),
+                              Icon(
+                                cubit.restaurant.rating > 1
+                                    ? Icons.star_rounded
+                                    : Icons.star_outline,
+                                size: 12,
+                                color: Colors.amber,
+                              ),
+                              Icon(
+                                cubit.restaurant.rating > 2
+                                    ? Icons.star_rounded
+                                    : Icons.star_outline,
+                                size: 12,
+                                color: Colors.amber,
+                              ),
+                              Icon(
+                                cubit.restaurant.rating > 3
+                                    ? Icons.star_rounded
+                                    : Icons.star_outline,
+                                size: 12,
+                                color: Colors.amber,
+                              ),
+                              Icon(
+                                cubit.restaurant.rating > 4
+                                    ? Icons.star_rounded
+                                    : Icons.star_outline,
+                                size: 12,
+                                color: Colors.amber,
+                              ),
+                            ],
+                          ),
+                          Text(
+                              '${cubit.customerReviews.length.toString()} ratings')
+                        ],
+                      ),
+                      const VerticalDivider(
+                        thickness: 2,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            buildRatingSummary(5, Random().nextDouble()),
+                            buildRatingSummary(4, Random().nextDouble()),
+                            buildRatingSummary(3, Random().nextDouble()),
+                            buildRatingSummary(2, Random().nextDouble()),
+                            buildRatingSummary(1, Random().nextDouble()),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Customer Review',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-                TextButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        TextEditingController name = TextEditingController();
-                        TextEditingController review = TextEditingController();
-                        return AlertDialog(
-                          title: const Text('Tell us about the restaurant'),
-                          content: IntrinsicHeight(
-                            child: Column(
-                              children: [
-                                TextField(
-                                  controller: name,
-                                  decoration: const InputDecoration(
-                                    label: Text('Name'),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Customer Review',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineSmall!
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              TextEditingController name =
+                                  TextEditingController();
+                              TextEditingController review =
+                                  TextEditingController();
+                              return AlertDialog(
+                                title:
+                                    const Text('Tell us about the restaurant'),
+                                content: IntrinsicHeight(
+                                  child: Column(
+                                    children: [
+                                      TextField(
+                                        controller: name,
+                                        decoration: const InputDecoration(
+                                          label: Text('Name'),
+                                        ),
+                                      ),
+                                      TextField(
+                                        controller: review,
+                                        decoration: const InputDecoration(
+                                          label: Text('Review'),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                TextField(
-                                  controller: review,
-                                  decoration: const InputDecoration(
-                                    label: Text('Review'),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          actions: [
-                            TextButton(
-                                style: TextButton.styleFrom(
-                                    foregroundColor: Colors.red),
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('Cancel')),
-                            ElevatedButton(
-                                onPressed: () {
-                                  print(
-                                      'name: ${name.text} review: ${review.text}');
-                                  cubit.postRestaurantReview(
-                                      widget.restaurant.id,
-                                      name.text,
-                                      review.text);
-                                },
-                                child: const Text(
-                                  'Add Review',
-                                )),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  child: const Text('Add Review'),
-                ),
-              ],
-            ),
-            Expanded(
-              child: ListView.separated(
-                itemCount: widget.restaurant.customerReviews.length,
-                separatorBuilder: (BuildContext context, int index) {
-                  return const Divider();
-                },
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                        child: Text(widget
-                            .restaurant.customerReviews[index].name[0]
-                            .toUpperCase())),
-                    title:
-                        Text(widget.restaurant.customerReviews[index].review),
-                    subtitle: Text(
-                        "${widget.restaurant.customerReviews[index].name} - ${widget.restaurant.customerReviews[index].date}"),
-                  );
-                },
+                                actions: [
+                                  TextButton(
+                                      style: TextButton.styleFrom(
+                                          foregroundColor: Colors.red),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('Cancel')),
+                                  ElevatedButton(
+                                      onPressed: () {
+                                        cubit.postRestaurantReview(
+                                            cubit.restaurant.id,
+                                            name.text,
+                                            review.text);
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: const Text(
+                                        'Add Review',
+                                      )),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text('Add Review'),
+                      ),
+                    ],
+                  ),
+                  buildCustomerReviews(),
+                ],
+              ),
+            );
+          }
+          return Container();
+        }),
+      ),
+    );
+  }
+
+  Widget buildCustomerReviews() {
+    return Expanded(
+      child: ListView.separated(
+        itemCount: cubit.customerReviews.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: CircleAvatar(
+              child: Text(
+                cubit.customerReviews[index].name[0].toUpperCase(),
               ),
             ),
-          ],
-        ),
+            title: Text(cubit.customerReviews[index].review),
+            subtitle: Text(
+                "${cubit.customerReviews[index].name} - ${cubit.customerReviews[index].date}"),
+          );
+        },
       ),
     );
   }
