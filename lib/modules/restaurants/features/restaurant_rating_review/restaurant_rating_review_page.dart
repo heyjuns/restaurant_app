078862ae95e8d95c108ab48825406beb1e291f07
Juanda <heyjuns@icloud.com>
2023-05-23 23:53:48 +0700

    update

--- lib/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_page.dart
+++ lib/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:restaurant_app/modules/restaurants/domain/entities/restaurant_detail_entity.dart';
 import 'package:restaurant_app/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_cubit.dart';
 import 'package:restaurant_app/modules/restaurants/features/restaurant_rating_review/restaurant_rating_review_state.dart';
+import 'package:restaurant_app/modules/restaurants/widgets/no_internet_widget.dart';
 
 import '../../data/repositories/restaurant_impl.dart';
 
@@ -40,165 +41,188 @@ class _RestaurantRatingReviewPageState
       ),
       body: BlocProvider(
         create: (context) => cubit,
-        child: BlocBuilder<RestaurantRatingReviewCubit,
-            RestaurantRatingReviewState>(builder: (context, state) {
-          if (state is RestaurantRatingReviewLoadedState) {
-            return Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            cubit.restaurant.rating.toString(),
-                            style: Theme.of(context)
-                                .textTheme
-                                .headlineMedium!
-                                .copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black87),
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                cubit.restaurant.rating > 0
-                                    ? Icons.star_rounded
-                                    : Icons.star_outline,
-                                size: 12,
-                                color: Colors.amber,
-                              ),
-                              Icon(
-                                cubit.restaurant.rating > 1
-                                    ? Icons.star_rounded
-                                    : Icons.star_outline,
-                                size: 12,
-                                color: Colors.amber,
-                              ),
-                              Icon(
-                                cubit.restaurant.rating > 2
-                                    ? Icons.star_rounded
-                                    : Icons.star_outline,
-                                size: 12,
-                                color: Colors.amber,
-                              ),
-                              Icon(
-                                cubit.restaurant.rating > 3
-                                    ? Icons.star_rounded
-                                    : Icons.star_outline,
-                                size: 12,
-                                color: Colors.amber,
-                              ),
-                              Icon(
-                                cubit.restaurant.rating > 4
-                                    ? Icons.star_rounded
-                                    : Icons.star_outline,
-                                size: 12,
-                                color: Colors.amber,
-                              ),
-                            ],
-                          ),
-                          Text(
-                              '${cubit.customerReviews.length.toString()} ratings')
-                        ],
-                      ),
-                      const VerticalDivider(
-                        thickness: 2,
-                      ),
-                      Expanded(
-                        child: Column(
+        child: MultiBlocListener(
+          listeners: [
+            BlocListener<RestaurantRatingReviewCubit,
+                RestaurantRatingReviewState>(
+              listener: (context, state) {
+                if (state is RestaurantRatingReviewErrorState) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("Failed to add a review"),
+                  ));
+                }
+
+                if (state is RestaurantRatingReviewLoadedState) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text("successfully add a review"),
+                  ));
+                }
+              },
+            )
+          ],
+          child: BlocBuilder<RestaurantRatingReviewCubit,
+              RestaurantRatingReviewState>(builder: (context, state) {
+            if (state is RestaurantRatingReviewLoadedState) {
+              return Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            buildRatingSummary(5, Random().nextDouble()),
-                            buildRatingSummary(4, Random().nextDouble()),
-                            buildRatingSummary(3, Random().nextDouble()),
-                            buildRatingSummary(2, Random().nextDouble()),
-                            buildRatingSummary(1, Random().nextDouble()),
+                            Text(
+                              cubit.restaurant.rating.toString(),
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headlineMedium!
+                                  .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black87),
+                            ),
+                            Row(
+                              children: [
+                                Icon(
+                                  cubit.restaurant.rating > 0
+                                      ? Icons.star_rounded
+                                      : Icons.star_outline,
+                                  size: 12,
+                                  color: Colors.amber,
+                                ),
+                                Icon(
+                                  cubit.restaurant.rating > 1
+                                      ? Icons.star_rounded
+                                      : Icons.star_outline,
+                                  size: 12,
+                                  color: Colors.amber,
+                                ),
+                                Icon(
+                                  cubit.restaurant.rating > 2
+                                      ? Icons.star_rounded
+                                      : Icons.star_outline,
+                                  size: 12,
+                                  color: Colors.amber,
+                                ),
+                                Icon(
+                                  cubit.restaurant.rating > 3
+                                      ? Icons.star_rounded
+                                      : Icons.star_outline,
+                                  size: 12,
+                                  color: Colors.amber,
+                                ),
+                                Icon(
+                                  cubit.restaurant.rating > 4
+                                      ? Icons.star_rounded
+                                      : Icons.star_outline,
+                                  size: 12,
+                                  color: Colors.amber,
+                                ),
+                              ],
+                            ),
+                            Text(
+                                '${cubit.customerReviews.length.toString()} ratings')
                           ],
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Customer Review',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              TextEditingController name =
-                                  TextEditingController();
-                              TextEditingController review =
-                                  TextEditingController();
-                              return AlertDialog(
-                                title:
-                                    const Text('Tell us about the restaurant'),
-                                content: IntrinsicHeight(
-                                  child: Column(
-                                    children: [
-                                      TextField(
-                                        controller: name,
-                                        decoration: const InputDecoration(
-                                          label: Text('Name'),
+                        const VerticalDivider(
+                          thickness: 2,
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              buildRatingSummary(5, Random().nextDouble()),
+                              buildRatingSummary(4, Random().nextDouble()),
+                              buildRatingSummary(3, Random().nextDouble()),
+                              buildRatingSummary(2, Random().nextDouble()),
+                              buildRatingSummary(1, Random().nextDouble()),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Customer Review',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                TextEditingController name =
+                                    TextEditingController();
+                                TextEditingController review =
+                                    TextEditingController();
+                                return AlertDialog(
+                                  title: const Text(
+                                      'Tell us about the restaurant'),
+                                  content: IntrinsicHeight(
+                                    child: Column(
+                                      children: [
+                                        TextField(
+                                          controller: name,
+                                          decoration: const InputDecoration(
+                                            label: Text('Name'),
+                                          ),
                                         ),
-                                      ),
-                                      TextField(
-                                        controller: review,
-                                        decoration: const InputDecoration(
-                                          label: Text('Review'),
+                                        TextField(
+                                          controller: review,
+                                          decoration: const InputDecoration(
+                                            label: Text('Review'),
+                                          ),
                                         ),
-                                      ),
-                                    ],
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                actions: [
-                                  TextButton(
-                                      style: TextButton.styleFrom(
-                                          foregroundColor: Colors.red),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: const Text('Cancel')),
-                                  ElevatedButton(
-                                      onPressed: () {
-                                        cubit.postRestaurantReview(
-                                            cubit.restaurant.id,
-                                            name.text,
-                                            review.text);
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text(
-                                        'Add Review',
-                                      )),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                        child: const Text('Add Review'),
-                      ),
-                    ],
-                  ),
-                  buildCustomerReviews(),
-                ],
-              ),
-            );
-          }
-          return Container();
-        }),
+                                  actions: [
+                                    TextButton(
+                                        style: TextButton.styleFrom(
+                                            foregroundColor: Colors.red),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('Cancel')),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          cubit.postRestaurantReview(
+                                              cubit.restaurant.id,
+                                              name.text,
+                                              review.text);
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text(
+                                          'Add Review',
+                                        )),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          child: const Text('Add Review'),
+                        ),
+                      ],
+                    ),
+                    buildCustomerReviews(),
+                  ],
+                ),
+              );
+            }
+            if (state is RestaurantRatingReviewNoInternetState) {
+              return const NoInternetWidget();
+            }
+            return Container();
+          }),
+        ),
       ),
     );
   }
