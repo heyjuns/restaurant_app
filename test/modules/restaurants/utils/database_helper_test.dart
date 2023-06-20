ab7a5d5e3039e6ff0519d4faa4b1700120037216
Juanda <heyjuns@icloud.com>
2023-06-21 00:40:32 +0700

    adjustment unit test

--- test/modules/restaurants/utils/database_helper_test.dart
+++ test/modules/restaurants/utils/database_helper_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:path/path.dart';
-import 'package:restaurant_app/modules/restaurants/domain/entities/restaurant_summary_entity.dart';
+import 'package:restaurant_app/modules/restaurants/domain/entities/menu_list_entity.dart';
+import 'package:restaurant_app/modules/restaurants/domain/entities/name_entity.dart';
+import 'package:restaurant_app/modules/restaurants/domain/entities/restaurant_detail_entity.dart';
 import 'package:restaurant_app/utils/database_helper.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
@@ -26,30 +28,56 @@ void main() {
     });
 
     test('Insert and retrieve a favorite restaurant', () async {
-      RestaurantSummaryEntity testRestaurant = const RestaurantSummaryEntity(
+      const menusMock = MenuListEntity(
+        foods: [
+          NameEntity(name: 'Food 1'),
+          NameEntity(name: 'Food 2'),
+        ],
+        drinks: [
+          NameEntity(name: 'Drink 1'),
+          NameEntity(name: 'Drink 2'),
+        ],
+      );
+      RestaurantDetailEntity testRestaurant = const RestaurantDetailEntity(
         id: 'test_id1',
         name: 'Test Restaurant',
         description: 'Test Description',
         pictureId: 'test_picture_id',
         city: 'Test City',
         rating: 4.5,
+        categories: [],
+        customerReviews: [],
+        menus: menusMock,
       );
       await databaseHelper.insertFavoriteRestaurant(testRestaurant);
 
       final retrievedRestaurant =
           await databaseHelper.getFavoriteRestaurantSummary(testRestaurant.id);
 
-      expect(retrievedRestaurant, testRestaurant);
+      expect(retrievedRestaurant.id, testRestaurant.id);
     });
 
     test('Check if a restaurant is favorited', () async {
-      RestaurantSummaryEntity testRestaurant = const RestaurantSummaryEntity(
+      const menus = MenuListEntity(
+        foods: [
+          NameEntity(name: 'Food 1'),
+          NameEntity(name: 'Food 2'),
+        ],
+        drinks: [
+          NameEntity(name: 'Drink 1'),
+          NameEntity(name: 'Drink 2'),
+        ],
+      );
+      RestaurantDetailEntity testRestaurant = const RestaurantDetailEntity(
         id: 'test_id2',
         name: 'Test Restaurant',
         description: 'Test Description',
         pictureId: 'test_picture_id',
         city: 'Test City',
         rating: 4.5,
+        categories: [],
+        customerReviews: [],
+        menus: menus,
       );
 
       await databaseHelper.insertFavoriteRestaurant(testRestaurant);
@@ -59,30 +87,55 @@ void main() {
       expect(isFavorite, true);
     });
     test('Retrieve all favorite restaurants', () async {
-      RestaurantSummaryEntity testRestaurant = const RestaurantSummaryEntity(
+      const menus = MenuListEntity(
+        foods: [
+          NameEntity(name: 'Food 1'),
+          NameEntity(name: 'Food 2'),
+        ],
+        drinks: [
+          NameEntity(name: 'Drink 1'),
+          NameEntity(name: 'Drink 2'),
+        ],
+      );
+      RestaurantDetailEntity testRestaurant = const RestaurantDetailEntity(
         id: 'test_id3',
         name: 'Test Restaurant',
         description: 'Test Description',
         pictureId: 'test_picture_id',
         city: 'Test City',
         rating: 4.5,
+        categories: [],
+        customerReviews: [],
+        menus: menus,
       );
-
       await databaseHelper.insertFavoriteRestaurant(testRestaurant);
 
       final favoriteRestaurants = await databaseHelper.getFavoriteRestaurants();
 
       expect(favoriteRestaurants.length, 3);
-      expect(favoriteRestaurants[2], testRestaurant);
+      expect(favoriteRestaurants[2].id, testRestaurant.id);
     });
     test('Delete a favorite restaurant', () async {
-      RestaurantSummaryEntity testRestaurant = const RestaurantSummaryEntity(
+      const menus = MenuListEntity(
+        foods: [
+          NameEntity(name: 'Food 1'),
+          NameEntity(name: 'Food 2'),
+        ],
+        drinks: [
+          NameEntity(name: 'Drink 1'),
+          NameEntity(name: 'Drink 2'),
+        ],
+      );
+      RestaurantDetailEntity testRestaurant = const RestaurantDetailEntity(
         id: 'test_id4',
         name: 'Test Restaurant',
         description: 'Test Description',
         pictureId: 'test_picture_id',
         city: 'Test City',
         rating: 4.5,
+        categories: [],
+        customerReviews: [],
+        menus: menus,
       );
       await databaseHelper.insertFavoriteRestaurant(testRestaurant);
 
